c1d2d52a807ebf52593bc8422cd9aab8275b19d0
smootbrain <87551908+smootbrain@users.noreply.github.com>
2022-01-15 15:37:54 -0700

    Deleted unnecessary index script file

---
+++ selectScript.js
@@ -0,0 +1,15 @@
+function renderParams() {
+
+}
+
+function renderRadius(parentNode) {
+    const container = document.createElement("div");
+    
+    const title = document.createElement("h2");
+    title.innerText = "radius";
+    container.appendChild(title);
+
+    const input = document.createElement("input");
+    input.setAttribute("type", )
+    
+}
