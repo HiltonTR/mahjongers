cef69af852eba377c8b8bdf5d4bc6a9ce9aa81ba
Nhat Huynh Do Minh <nhat.huynh-do-minh@ubisoft.com>
2022-01-15 15:06:41 -0700

    polish getting info from Yelp

--- getYelpInfo.py
+++ getYelpInfo.py
@@ -1,30 +1,51 @@
+
+from sre_parse import CATEGORIES
+from unicodedata import category
 import requests
 import json
-
+import enum
+# ------------------------ NECESSARY INFO TO USE YELP API ----------------------------------------
 api_key = '9BqKWX1mOXXiQqomscePk5OUJ4YhoI2BS-cNLAkQq63KTLoONoPxyvJ9YCDlCHRoskLeUSn70s4gCIrwrL6T3TgBiSo4o-bwqBFBcMmzUS3-tarHNs6Bhw8mWHziYXYx'
 headers = {'Authorization': 'Bearer %s' % api_key}
 
 url = 'https://api.yelp.com/v3/businesses/search'
+#yelp params: https://www.yelp.ca/developers/documentation/v3/business_search
+#categories can be Restaurants (restaurants, All), Food (food, All), Nightlife (nightlife, All)->Bars (bars, All)
+#category list: https://www.yelp.ca/developers/documentation/v3/all_category_list
+# ----------------------------------------------------------------------------------------------------
 
-params = {'term':'Cozy Bar','location':'Edmonton', 'radius':'40000', 'open_now':'true', 'limit':'50', } #if city is just searched take out radius term and add open_now = true 
+# --------PARAMS AREAS--------
+term = 'Cozy Bar';
+location = 'Edmonton';
+radius = '40000';
+available = 'true';
+limit = '10';
+categories = '';
+# -----------------------------
 
-req = requests.get(url, params=params, headers=headers)
+def getRestaurants():
+    params = {'term': term,'location': location, 'radius': radius, 'open_now': available, 'limit': limit} #if city is just searched take out radius term and add open_now = true 
+    req = requests.get(url, params=params, headers=headers)
+    parsedInfo = json.loads(req.text)
+    restaurants = parsedInfo["businesses"]
+    return restaurants
 
-yelpInfo = json.loads(req.text)
-# print(yelpInfo)
 
-restaurants = yelpInfo["businesses"]
-jsonDict= {};
-for restaurant in restaurants:
-    restaurantName = restaurant["name"];
-    restaurantRating = restaurant["rating"];
-    jsonDict [restaurantName] = restaurantRating;
-    # print("Name:", business["name"])
-    # print("Rating:", business["rating"])
-
-#print place name and rating to json
-def writeToJson(data, filename = "yelpRating.json"):
+# create json file containing place name, rating and address
+def writeToJson(filename = "yelpRating.json"):
+    restaurants = getRestaurants()
+    print(restaurants)
+    jsonObj = {}
+    jsonObj['restaurants'] = []
     with open(filename, "w") as file:
-        json.dump(data, file, indent = 4)
+        for restaurant in restaurants:
+            # print(restaurant)
+            restaurantName = restaurant["name"]
+            restaurantRating = restaurant["rating"]
+            restaurantAddress = restaurant["location"]["display_address"];
+            jsonLine = {'name': restaurantName, 'rating': restaurantRating, 'address': restaurantAddress}
+            jsonObj['restaurants'].append(jsonLine)
 
-writeToJson(jsonDict);
+        json.dump(jsonObj, file, indent = 4)
+
+writeToJson();
