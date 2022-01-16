767821ce30bdea2b0d2142860463c22809f6765c
smootbrain <87551908+smootbrain@users.noreply.github.com>
2022-01-15 20:38:39 -0700

    Added a city field to the select page

--- selectScript.js
+++ selectScript.js
@@ -1,7 +1,7 @@
 var output = document.getElementById("demo");
 var slider = document.getElementById("radius-slider").oninput = function() {
-
     output.innerHTML = `${this.value}` + " km";
 };
+
 
 
