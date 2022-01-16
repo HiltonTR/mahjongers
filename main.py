da0f2524fa19c32d4efa67a7c7268c64ff5443a9
HiltonTR <66839605+HiltonTR@users.noreply.github.com>
2022-01-15 18:03:06 -0700

    Update main.py

--- main.py
+++ main.py
@@ -7,17 +7,28 @@ import time
 
 from selenium import webdriver
 
-
+inputs = {
+    "term": "Japo",
+    "location": "Edmonton",
+    "radius": "40000",
+    "available": "true",
+    "limit": "10",
+    "categories": ""
+}
 
 def main():
+    writeToJson(inputs, "yelpRating.json")
+
     driver = webdriver.Chrome(os.getcwd() + r"/chromedriver_win32/chromedriver.exe")
     reviewsSearched, numberOfReviews, Rating = extract_google_reviews(driver, 'japonais')
-    driver.quit()
-        
+    driver.quit()    
     
     
     print(reviewsSearched)
     print(numberOfReviews)
     print(Rating)
+
+    getRestaurantOrder("Japonais", Rating, reviewsSearched)
+
 
 main()
